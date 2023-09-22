38f797d5ee2263420ed15d0e46bb1ad3e883fcbc
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-22 14:09:10 +0100

    Puppet Resource Delcaration

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# Using Puppet, create a manifest that kills a process named killmenow
+exec { 'Kill':
+  command     => '/usr/bin/pkill killmenow',
+  onlyif      => '/usr/bin/pgrep killmenow',
+  refreshonly => true,
+}
