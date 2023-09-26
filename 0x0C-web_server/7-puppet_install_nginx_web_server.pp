ef3f8365ff73b3e76412638cdfcd7d60127dcc7d
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-09-26 13:10:13 +0100

    Puppet install nginx web server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,27 +1,38 @@
-# Puppet manifest to install nginx
-# Requirement:
-# Nginx should be listening on port 80
-# When querying Nginx at its root / with a GET request
-# (requesting a page) using curl, it must return a page that
-# contains the string Holberton School
-# The redirection must be a “301 Moved Permanently”
+# Puppet manifest to install and configure Nginx
+# Requirements:
+# - Nginx should be listening on port 80
+# - When querying Nginx at its root / with a GET request, it must return a page that contains the string "Hello World!"
+# - The redirection must be a "301 Moved Permanently"
 
+# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'server_config':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School',
-}
-
+# Ensure Nginx service is running
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
+}
+
+# Define the Nginx site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('my_module/nginx.conf.erb'), # Use an ERB template for Nginx configuration
+  notify  => Service['nginx'], # Reload Nginx when the configuration changes
+}
+
+# Create the root page
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+}
+
+# Define the redirection for /redirect_me
+nginx::resource::location { 'redirect_me':
+  ensure       => present,
+  location     => '/redirect_me',
+  rewrite_rule => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require      => Package['nginx'],
+  notify       => Service['nginx'], # Reload Nginx when the configuration changes
 }
