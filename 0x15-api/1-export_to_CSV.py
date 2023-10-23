2152184f230eaad2cb4f9ce58a12932ca835963b
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-10-23 13:43:36 +0100

    Python script that Pings a To-Do API for data on a specified user and saves the data to a CSV file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Pings a To-Do API for data on a specified user,
+saves the data to a CSV file
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    user_id = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(user_id), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(user_id), verify=False).json()
+
+    with open("{}.csv".format(user_id), 'w', newline='') as csv_file:
+        csvwriter = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for task in todo:
+            csvwriter.writerow([int(user_id), user.get('username'),
+                            task.get('completed'),
+                            task.get('title')])
