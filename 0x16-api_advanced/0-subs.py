67fc269a0e467cdf817eb70ffa85dda1a259c344
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-11-07 13:44:04 +0100

    Function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,13 +4,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    user_agent = {'User-Agent': 'maonimisi'}
-    request = requests.get(url, headers=user_agent, allow_redirects=False)
-    if request.status_code == 200:
-        request = request.json()
-        data = request.get('data')
-        subs = data.get('subscribers')
-        return subs
-    return 0
+    user_agent = {'User-Agent': 'Maonimisi'}
+
+    response = requests.get(url, headers=user_agent, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
+        return 0
