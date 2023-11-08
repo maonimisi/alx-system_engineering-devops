aaffe53d26d27b1fc5934e16dc8e4411b5d38da6
Muhammed Abdulrasheed Onimisi <maonimisi>
2023-11-08 06:54:34 +0100

    Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-
+"""Queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit
+"""
 import requests
 
 
 def top_ten(subreddit):
-    '''function that queries the Reddit API and prints the titles
-       of the first 10 hot posts listed for a given subreddit
-    '''
+    """Prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
 
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
+    base_url = 'https://www.reddit.com'
+    api_uri = '{base}/r/{subreddit}/hot.json'.format(base=base_url,
+                                                     subreddit=subreddit)
 
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    user_agent = {'User-Agent': 'Python/requests'}
+
+    payload = {'limit': '10'}
+
+    response = requests.get(api_uri, headers=user_agent,
+                            params=payload, allow_redirects=False)
+
+    if response.status_code in [302, 404]:
+        print('None')
     else:
-        print(None)
+        response_json = response.json()
+
+        if (response_json.get('data') and
+           response_json.get('data').get('children')):
+            top_posts = response_json.get('data').get('children')
+
+            for post in top_posts:
+                if post.get('data') and post.get('data').get('title'):
+                    print(post.get('data').get('title'))
